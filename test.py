cafe1cc7904e1b99d6be802c9d5f0941904456d6
kai011x <kai011x@naver.com>
2024-05-15 22:20:13 +0900

    commit1

--- test.py
+++ test.py
@@ -1,17 +1,3 @@
 2018312626
 opensource_software
-<<<<<<< HEAD
-<<<<<<< HEAD
-Hello world
-Good Morning
-<<<<<<< HEAD
-Good Night
-My name is Kimyoungkyun
-=======
->>>>>>> parent of a9b97bc (Revert Test2)
-=======
->>>>>>> parent of 7ef5034 (Hello world)
 
-=======
-KimYoungkyun
->>>>>>> parent of 7ae62f5 (Second Commit Test)
